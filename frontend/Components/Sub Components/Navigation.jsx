2bf9820c6b3a76cdd3f3aa8c7f048fdee4fb8da8
Afzal <sayyed0786afzal@gmail.com>
2021-09-06 12:45:59 +0530

    importing file form other folder done, created 2 new files " Home & Navigation" exported Home from app. js and its working fine.

---
+++ frontend/Components/Sub Components/Navigation.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { styling } from "../../Configs/Style/Style";
+import { View, Text } from "react-native";
+
+export default function Navigation() {
+	return (
+		<View>
+			<Text style={styling.text}>helllllllloooooo guyzzzzz</Text>
+		</View>
+	);
+}

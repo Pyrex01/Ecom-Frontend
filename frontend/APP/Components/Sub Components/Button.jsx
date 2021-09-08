bc4700ec0eabd7163af10b016dafec9578cd6c62
Tazeen Shaikh <tazeenshaikh0114@gmail.com>
2021-09-08 22:36:33 +0530

    created Props For Button And An OnBoardScreen

--- frontend/APP/Components/Sub Components/Button.jsx
+++ frontend/APP/Components/Sub Components/Button.jsx
@@ -1,37 +1,40 @@
-import React from 'react';
-import {StyleSheet, TouchableOpacity, View, Text} from 'react-native';
-import COLORS from '../../Configs/Colors/Colors';
+import React from "react";
+import { StyleSheet, TouchableOpacity, View, Text } from "react-native";
+import COLORS from "../../Configs/Colors/Colors";
 
-const PrimaryButton = ({title, onPress = () => {}}) => {
-  return (
-    <TouchableOpacity activeOpacity={0.8} onPress={onPress}>
-      <View style={style.btnContainer}>
-        <Text style={style.title}>{title}</Text>
-      </View>
-    </TouchableOpacity>
-  );
+const PrimaryButton = ({ title, onPress = () => {} }) => {
+	return (
+		<TouchableOpacity activeOpacity={0.8} onPress={onPress}>
+			<View style={style.btnContainer}>
+				<Text style={style.title}>{title}</Text>
+			</View>
+		</TouchableOpacity>
+	);
 };
-const SecondaryButton = ({title, onPress = () => {}}) => {
-  return (
-    <TouchableOpacity activeOpacity={0.8} onPress={onPress}>
-      <View style={{...style.btnContainer, backgroundColor: COLORS.white}}>
-        <Text style={{...style.title, color: COLORS.primary}}>{title}</Text>
-      </View>
-    </TouchableOpacity>
-  );
+const SecondaryButton = ({ title, onPress = () => {} }) => {
+	return (
+		<TouchableOpacity activeOpacity={0.8} onPress={onPress}>
+			<View
+				style={{ ...style.btnContainer, backgroundColor: COLORS.white }}
+			>
+				<Text style={{ ...style.title, color: COLORS.primary }}>
+					{title}
+				</Text>
+			</View>
+		</TouchableOpacity>
+	);
 };
 
 const style = StyleSheet.create({
-  title: {color: COLORS.white, fontWeight: 'bold', fontSize: 18},
-   btnContainer: {
-    backgroundColor: COLORS.primary,
-    height: 60,
-    borderRadius: 30,
-    top:-150,
-    justifyContent: 'center',
-    alignItems: 'center',
-    
-  },
+	title: { color: COLORS.white, fontWeight: "bold", fontSize: 18 },
+	btnContainer: {
+		backgroundColor: COLORS.primary,
+		height: 60,
+		borderRadius: 30,
+		top: -150,
+		justifyContent: "center",
+		alignItems: "center",
+	},
 });
 
-export {PrimaryButton, SecondaryButton};
+export { PrimaryButton, SecondaryButton };

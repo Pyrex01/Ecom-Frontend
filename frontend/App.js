148944efe7272b3f05996453876a77ee05ac7b3e
Afzal <sayyed0786afzal@gmail.com>
2021-09-08 23:45:34 +0530

    changes in app.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,13 +1,15 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import OnBoardScreen from './APP/Components/Screens/OnBoardScreen';
-
+import "react-native-gesture-handler";
+import React from "react";
+import OnBoardScreen from "./APP/Components/Screens/OnBoardScreen";
+import Home from "./APP/Components/Screens/Home";
+import { View } from "react-native";
 
 const App = () => {
-  return (
-   <OnBoardScreen/>
-  );
+	return (
+		<View>
+			<OnBoardScreen />,
+		</View>
+	);
 };
 
 export default App;
-

74e6855f50908ffa3a64d3e755b8c7bef7917293
Afzal <sayyed0786afzal@gmail.com>
2021-09-08 23:42:51 +0530

    corrected Home.jsx file

--- frontend/APP/Components/Screens/Home.jsx
+++ frontend/APP/Components/Screens/Home.jsx
@@ -1,50 +1,38 @@
-// import { StatusBar } from "expo-status-bar";
+import { StatusBar } from "expo-status-bar";
 import React from "react";
-// import Bgimg from "../../images/Background.jpeg";
-// import Navbar from "../Sub Components/Navbar";
-// import Icons from "../../assets/favicon.png";
-// import { Styling } from "../../Configs/Style/Style";
-// import { Button, Image, ImageBackground, Text, View } from "react-native";
-// import { View } from "react-native";
-import {PrimaryButton } from "../Sub Components/Button";
-import { Button, View } from "react-native";
+import Bgimg from "../../images/Background.jpeg";
+import Navbar from "../Sub Components/Navbar";
+import Icons from "../../assets/favicon.png";
+import { Styling } from "../../Configs/Style/Style";
+import { Image, ImageBackground, Text, View } from "react-native";
 
-<<<<<<< HEAD
-const Home = ({}) => {
-=======
 export default function Home() {
 	console.log(Bgimg);
->>>>>>> 8ead5e80816a8ce43ba1a2194e4018a36c2022e6
 	return (
-		// <View style={Styling.container}>
-		// 	<ImageBackground
-		// 		source={Bgimg}
-		// 		style={(Styling.Bgimg, Styling.container)}
-		// 	>
-		// 		<Image source={Icons} style={Styling.icon} />
-		// 		<Text style={Styling.text}>Hello Darkness my Ol friend</Text>
-		// 		<Text style={Styling.text}>
-		// 			lOVcGUoRxcEcfyvFMAnDgaF1VWwl Lorem Ipsum is simply dummy
-		// 			text of the printing and typesetting industry. Lorem Ipsum
-		// 			has been the industry's standard dummy text ever since the
-		// 			1500s, when an unknown printer took a galley of type and
-		// 			scrambled it to make a type specimen book. It has survived
-		// 			not only five centuries, but also the leap into electronic
-		// 			typesetting, remaining essentially unchanged. It was
-		// 			popularised in the 1960s with the release of Letraset sheets
-		// 			containing Lorem Ipsum passages, and more recently with
-		// 			desktop publishing software like Aldus PageMaker including
-		// 			versions of Lorem Ipsum.
-		// 		</Text>
-		// 		<Navbar />
+		<View style={Styling.container}>
+			<ImageBackground
+				source={Bgimg}
+				style={(Styling.Bgimg, Styling.container)}
+			>
+				<Image source={Icons} style={Styling.icon} />
+				<Text style={Styling.text}>Hello Darkness my Ol friend</Text>
+				<Text style={Styling.text}>
+					lOVcGUoRxcEcfyvFMAnDgaF1VWwl Lorem Ipsum is simply dummy
+					text of the printing and typesetting industry. Lorem Ipsum
+					has been the industry's standard dummy text ever since the
+					1500s, when an unknown printer took a galley of type and
+					scrambled it to make a type specimen book. It has survived
+					not only five centuries, but also the leap into electronic
+					typesetting, remaining essentially unchanged. It was
+					popularised in the 1960s with the release of Letraset sheets
+					containing Lorem Ipsum passages, and more recently with
+					desktop publishing software like Aldus PageMaker including
+					versions of Lorem Ipsum.
+				</Text>
+				<Navbar />
 
-		// 		<StatusBar style="auto" />
-		// 	</ImageBackground>
-		// </View>
-		<View>
-			<PrimaryButton title="Click Me"/>
-			<Button/>
+				<StatusBar style="auto" />
+			</ImageBackground>
 		</View>
 	);
 }
-export default Home;

fae5a26b4ca6aee5899f08b1b13858a04bf60aba
riyan <khanshafique.ahamed@gmail.com>
2021-09-09 12:10:26 +0530

    added different components for each collecetion of table and address table done so far

---
+++ backend/addressCollection/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from django.db.models.fields.related import ForeignKey
+
+class Address_types(models.Model):
+    Address_type = models.CharField(max_length=15)
+
+
+
+
+class Address(models.Model):
+    STATES = [
+            ('AP',"Andhra Pradesh"),
+            ('AR',"Arunachal Pradesh"),
+            ('AS',"Assam"),
+            ('BR',"Bihar"),
+            ('CT',"Chhattisgarh"),
+            ('GA',"Goa"),
+            ('GJ',"Gujarat"),
+            ('HR',"Haryana"),
+            ('HP',"Himachal Pradesh"),
+            ('JH',"Jharkhand"),
+            ('KA',"Karnataka"),
+            ('KL',"Kerala"),
+            ('MP',"Madhya Pradesh"),
+            ('MH',"Maharashtra"),
+            ('MN',"Manipur"),
+            ('ML',"Meghalaya"),
+            ('MZ',"Mizoram"),
+            ('NL',"Nagaland"),
+            ('OR',"Orissa, Odisha"),
+            ('PB',"Punjab, Punjab (India)"),
+            ('RJ',"Rajasthan"),
+            ('SK',"Sikkim"),
+            ('TN',"Tamil Nadu, Tamizh Nadu"),
+            ('TG',"Telangana"),
+            ('TR',"Tripura"),
+            ('UL',"Uttarakhand"),
+            ('UP',"Uttar Pradesh"),
+            ('WB',"West Bengal"),
+            ('AN',"Andaman and Nicobar Islands"),
+            ('CH',"Chandigarh"),
+            ('DN',"Dadra and Nagar Haveli, Dadra & Nagar Haveli"),
+            ('DD',"Daman and Diu"),
+            ('DL',"Delhi, National Capital Territory of Delhi"),
+            ('JK',"Jammu and Kashmir"),
+            ('LA',"Ladakh"),
+            ('LD',"Lakshadweep"),
+            ('PY',"Pondicherry, Puducherry"),
+    ]
+
+    Name = models.CharField(max_length=30)
+    Phone_number = models.CharField(max_length=13)
+    Pincode = models.IntegerField()
+    Regein = models.TextField()
+    Landmark = models.TextField()
+    Town = models.TextField()
+    State = models.CharField(max_length=2,choices=STATES)
+    Address_type_ID = ForeignKey(Address_types)    

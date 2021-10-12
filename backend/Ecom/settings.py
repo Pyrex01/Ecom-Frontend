871e7ef6d6c3e455e5a5d7fb0d6f735794fdf427
riyan <khanshafique.ahamed@gmail.com>
2021-10-12 09:02:18 +0530

    added cors headers

--- backend/Ecom/settings.py
+++ backend/Ecom/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'userManagement.apps.UsermanagementConfig',
     'store.apps.StoreConfig',
     'addressCollection.apps.AddresscollectionConfig',
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'Ecom.urls'
@@ -135,3 +137,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ORIGIN_ALLOW_ALL = True

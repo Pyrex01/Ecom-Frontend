392c223eda3b91d0e5c80b4073e805551fb20c0f
riyan <khanshafique.ahamed@gmail.com>
2021-10-29 20:08:34 +0530

    added items pagination of main page display

--- backend/Ecom/urls.py
+++ backend/Ecom/urls.py
@@ -18,5 +18,6 @@ from django.urls import path , include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("user/",include("userManagement.urls")),
-    path("docs/",include("docs.urls"))
+    path("docs/",include("docs.urls")),
+    path("",include("store.urls"))
 ]

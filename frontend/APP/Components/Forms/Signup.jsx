45b9f6877a2a8b0b454c31b91ba81b038b5cca22
Afzal <sayyed0786afzal@gmail.com>
2021-10-29 20:59:43 +0530

    merge

--- frontend/APP/Components/Forms/Signup.jsx
+++ frontend/APP/Components/Forms/Signup.jsx
@@ -17,7 +17,7 @@ import {
 	Picker,
 	Modal,
 	Button
-	
+
 } from "react-native";
 import * as axios from 'axios';
 
@@ -43,9 +43,9 @@ const Test2 = ({ navigation }) => {
 	let [genderlog, setGenderLog] = useState("");
 	let [passwordlog, setPasswordLog] = useState("");
 	let [confirm_passwordlog, setConfirmPasswordLog] = useState("");
-	let [otp,SetOtp] = useState("");
-	let [otpwarning,Setotpwarning] = useState("");
-	let [isVisible,SetIsVisible] = useState(false);
+	let [otp, SetOtp] = useState("");
+	let [otpwarning, Setotpwarning] = useState("");
+	let [isVisible, SetIsVisible] = useState(true);
 	// const [selectedValue, setSelectedValue] = useState("1,2,0");
 	let user_Data = {
 		first_name,
@@ -71,133 +71,133 @@ const Test2 = ({ navigation }) => {
 
 	return (
 		<>
-		<ScrollView>
-			<SafeAreaView
-				style={{
-					paddingHorizontal: 40,
-					flex: 1,
-					backgroundColor: Colors.white,
-				}}
-			>
+			<ScrollView>
 				<SafeAreaView
-					style={{ backgroundColor: Colors.white, flex: 1 }}
+					style={{
+						paddingHorizontal: 40,
+						flex: 1,
+						backgroundColor: Colors.white,
+					}}
 				>
-					<View style={style.header}>
-						<Icon
-							name="arrow-back-ios"
-							size={28}
-							onPress={navigation.goBack}
-						/>
-						<Text style={{ fontSize: 20, fontWeight: "bold" }}>
-							SignUp
+					<SafeAreaView
+						style={{ backgroundColor: Colors.white, flex: 1 }}
+					>
+						<View style={style.header}>
+							<Icon
+								name="arrow-back-ios"
+								size={28}
+								onPress={navigation.goBack}
+							/>
+							<Text style={{ fontSize: 20, fontWeight: "bold" }}>
+								SignUp
+							</Text>
+						</View>
+					</SafeAreaView>
+					<View style={{ flexDirection: "row", marginTop: 60 }}>
+						<Text
+							style={{
+								fontWeight: "bold",
+								fontSize: 32,
+								color: Colors.dark,
+							}}
+						>
+							Shopping
+						</Text>
+						<Text
+							style={{
+								fontWeight: "bold",
+								fontSize: 32,
+								color: Colors.secondary,
+							}}
+						>
+							Bazaar
 						</Text>
 					</View>
-				</SafeAreaView>
-				<View style={{ flexDirection: "row", marginTop: 60 }}>
-					<Text
-						style={{
-							fontWeight: "bold",
-							fontSize: 32,
-							color: Colors.dark,
-						}}
-					>
-						Shopping
-					</Text>
-					<Text
-						style={{
-							fontWeight: "bold",
-							fontSize: 32,
-							color: Colors.secondary,
-						}}
-					>
-						Bazaar
-					</Text>
-				</View>
 
-				<View style={{ marginTop: 40 }}>
-					<Text
-						style={{
-							fontSize: 42,
-							textAlign: "center",
-							fontWeight: "bold",
-							color: Colors.dark,
-						}}
-					>
-						Welcome...
-					</Text>
-					<Text
-						style={{
-							fontSize: 19,
-							textAlign: "center",
-							fontWeight: "bold",
-							color: Colors.grey,
-						}}
-					>
-						Sign up to continue
-					</Text>
-				</View>
-				<View style={{ marginTop: 20 }}>
-					<View style={STYLES.inputContainer}>
-						<Icon
-							name="person-outline"
-							color={Colors.grey}
-							size={20}
-							style={STYLES.inputIcon}
-						/>
-
-						<TextInput
-							name="first_name"
-							placeholder="First Name"
-							onChangeText={text => setFirstName(text)}
-							// values={values.first_name}
-							style={STYLES.input}
-						/>
-					</View>
-					<View>
-						<Text>{first_namelog}</Text>
-					</View>
-					<View style={STYLES.inputContainer}>
-						<Icon
-							name="person-outline"
-							color={Colors.grey}
-							size={20}
-							style={STYLES.inputIcon}
-						/>
-
-						<TextInput
-							name="last_name"
-							placeholder="Last Name"
-							onChangeText={text => setLastName(text)}
-							// values={values.last_name}
-							style={STYLES.input}
-						/>
-						{/* <Text ref={NameError}> </Text> */}
-					</View>
-					<View>
-						<Text>{last_namelog}</Text>
-					</View>
-					<View style={STYLES.inputContainer}>
-						<Icon
-							name="person-outline"
-							color={Colors.grey}
-							size={20}
-							style={STYLES.inputIcon}
-						/>
-
-						<Picker
-							selectedValue={gender}
-							style={{ height: 50, width: 150 }}
-							onValueChange={(itemValue, itemIndex) =>
-								setGender(itemValue)
-							}
+					<View style={{ marginTop: 40 }}>
+						<Text
+							style={{
+								fontSize: 42,
+								textAlign: "center",
+								fontWeight: "bold",
+								color: Colors.dark,
+							}}
 						>
-							<Picker.Item label="Select" value="" />
-							<Picker.Item label="Male" value="1" />
-							<Picker.Item label="Female" value="2" />
-							<Picker.Item label="Others" value="0" />
-						</Picker>
+							Welcome...
+						</Text>
+						<Text
+							style={{
+								fontSize: 19,
+								textAlign: "center",
+								fontWeight: "bold",
+								color: Colors.grey,
+							}}
+						>
+							Sign up to continue
+						</Text>
+					</View>
+					<View style={{ marginTop: 20 }}>
+						<View style={STYLES.inputContainer}>
+							<Icon
+								name="person-outline"
+								color={Colors.grey}
+								size={20}
+								style={STYLES.inputIcon}
+							/>
 
-						{/* <TextInput
+							<TextInput
+								name="first_name"
+								placeholder="First Name"
+								onChangeText={text => setFirstName(text)}
+								// values={values.first_name}
+								style={STYLES.input}
+							/>
+						</View>
+						<View>
+							<Text>{first_namelog}</Text>
+						</View>
+						<View style={STYLES.inputContainer}>
+							<Icon
+								name="person-outline"
+								color={Colors.grey}
+								size={20}
+								style={STYLES.inputIcon}
+							/>
+
+							<TextInput
+								name="last_name"
+								placeholder="Last Name"
+								onChangeText={text => setLastName(text)}
+								// values={values.last_name}
+								style={STYLES.input}
+							/>
+							{/* <Text ref={NameError}> </Text> */}
+						</View>
+						<View>
+							<Text>{last_namelog}</Text>
+						</View>
+						<View style={STYLES.inputContainer}>
+							<Icon
+								name="person-outline"
+								color={Colors.grey}
+								size={20}
+								style={STYLES.inputIcon}
+							/>
+
+							<Picker
+								selectedValue={gender}
+								style={{ height: 50, width: 150 }}
+								onValueChange={(itemValue, itemIndex) =>
+									setGender(itemValue)
+								}
+							>
+								<Picker.Item label="Select" value="" />
+								<Picker.Item label="Male" value="1" />
+								<Picker.Item label="Female" value="2" />
+								<Picker.Item label="Others" value="0" />
+							</Picker>
+
+							{/* <TextInput
 							name="gender"
 							placeholder="Gender"
 							onChangeText={text => setGender(text)}
@@ -205,211 +205,211 @@ const Test2 = ({ navigation }) => {
 							style={STYLES.input}
 						/>
 						<Text ref={NameError}> </Text> */}
-					</View>
-					<View>
-						<Text>{genderlog}</Text>
-					</View>
-					<View style={STYLES.inputContainer}>
-						<Icon
-							name="phone"
-							color={Colors.grey}
-							size={20}
-							style={STYLES.inputIcon}
-						/>
-
-						<TextInput
-							name="phone"
-							placeholder="Phone"
-							onChangeText={text => setPhone(text)}
-							// values={values.phone}
-							style={STYLES.input}
-							autoCompleteType="tel"
-							keyboardType="number-pad"
-						/>
-					</View>
-					<View>
-						<Text>{phonelog}</Text>
-					</View>
-					<View style={STYLES.inputContainer}>
-						<Icon
-							name="mail-outline"
-							color={Colors.grey}
-							size={20}
-							style={STYLES.inputIcon}
-						/>
-						<TextInput
-							name="email"
-							placeholder="Email"
-							onChangeText={text => setEmail(text)}
-							// values={values.email}
-							style={STYLES.input}
-							keyboardType="email-address"
-							autoCompleteType="email"
-							autoCorrect={false}
-							autoCapitalize="none"
-							textContentType="emailAddress"
-						/>
-					</View>
-					<View>
-						<Text>{emaillog}</Text>
-					</View>
-					<View style={STYLES.inputContainer}>
-						<Icon
-							name="lock-outline"
-							color={Colors.grey}
-							size={20}
-							style={STYLES.inputIcon}
-						/>
-						<TextInput
-							name="password"
-							placeholder="Password"
-							style={STYLES.input}
-							secureTextEntry
-							onChangeText={text => setPassword(text)}
-							// values={values.password}
-							autoCorrect={false}
-							autoCapitalize="none"
-							textContentType="Password"
-							autoCompleteType="password"
-						/>
-					</View>
-					<View>
-						<Text>{passwordlog}</Text>
-					</View>
-					<View style={STYLES.inputContainer}>
-						<Icon
-							name="lock-outline"
-							color={Colors.grey}
-							size={20}
-							style={STYLES.inputIcon}
-						/>
-						<TextInput
-							name="confirm_password"
-							placeholder="Confirm Password"
-							style={STYLES.input}
-							secureTextEntry
-							onChangeText={text => setConfirm_Password(text)}
-							// values={values.confirm_password}
-							autoCorrect={false}
-							autoCapitalize="none"
-							textContentType="Password"
-							autoCompleteType="password"
-						/>
-					</View>
-					<View>
-						<Text>{confirm_passwordlog}</Text>
-					</View>
-					<TouchableOpacity>
-						<View style={STYLES.btnPrimary}>
-							<Text
-								style={{
-									color: "#fff",
-									fontWeight: "bold",
-									fontSize: 20,
-								}}
-								onPress={() => submit(user_Data, log_Setters)}
-							>
-								Sign Up
-							</Text>
 						</View>
-					</TouchableOpacity>
-					<View
-						style={{
-							marginVertical: 20,
-							flexDirection: "row",
-							justifyContent: "center",
-							alignItems: "center",
-						}}
-					>
-						<View style={STYLES.line}></View>
-						<Text
-							style={{ marginHorizontal: 5, fontWeight: "bold" }}
+						<View>
+							<Text>{genderlog}</Text>
+						</View>
+						<View style={STYLES.inputContainer}>
+							<Icon
+								name="phone"
+								color={Colors.grey}
+								size={20}
+								style={STYLES.inputIcon}
+							/>
+
+							<TextInput
+								name="phone"
+								placeholder="Phone"
+								onChangeText={text => setPhone(text)}
+								// values={values.phone}
+								style={STYLES.input}
+								autoCompleteType="tel"
+								keyboardType="number-pad"
+							/>
+						</View>
+						<View>
+							<Text>{phonelog}</Text>
+						</View>
+						<View style={STYLES.inputContainer}>
+							<Icon
+								name="mail-outline"
+								color={Colors.grey}
+								size={20}
+								style={STYLES.inputIcon}
+							/>
+							<TextInput
+								name="email"
+								placeholder="Email"
+								onChangeText={text => setEmail(text)}
+								// values={values.email}
+								style={STYLES.input}
+								keyboardType="email-address"
+								autoCompleteType="email"
+								autoCorrect={false}
+								autoCapitalize="none"
+								textContentType="emailAddress"
+							/>
+						</View>
+						<View>
+							<Text>{emaillog}</Text>
+						</View>
+						<View style={STYLES.inputContainer}>
+							<Icon
+								name="lock-outline"
+								color={Colors.grey}
+								size={20}
+								style={STYLES.inputIcon}
+							/>
+							<TextInput
+								name="password"
+								placeholder="Password"
+								style={STYLES.input}
+								secureTextEntry
+								onChangeText={text => setPassword(text)}
+								// values={values.password}
+								autoCorrect={false}
+								autoCapitalize="none"
+								textContentType="Password"
+								autoCompleteType="password"
+							/>
+						</View>
+						<View>
+							<Text>{passwordlog}</Text>
+						</View>
+						<View style={STYLES.inputContainer}>
+							<Icon
+								name="lock-outline"
+								color={Colors.grey}
+								size={20}
+								style={STYLES.inputIcon}
+							/>
+							<TextInput
+								name="confirm_password"
+								placeholder="Confirm Password"
+								style={STYLES.input}
+								secureTextEntry
+								onChangeText={text => setConfirm_Password(text)}
+								// values={values.confirm_password}
+								autoCorrect={false}
+								autoCapitalize="none"
+								textContentType="Password"
+								autoCompleteType="password"
+							/>
+						</View>
+						<View>
+							<Text>{confirm_passwordlog}</Text>
+						</View>
+						<TouchableOpacity>
+							<View style={STYLES.btnPrimary}>
+								<Text
+									style={{
+										color: "#fff",
+										fontWeight: "bold",
+										fontSize: 20,
+									}}
+									onPress={() => submit(user_Data, log_Setters)}
+								>
+									Sign Up
+								</Text>
+							</View>
+						</TouchableOpacity>
+						<View
+							style={{
+								marginVertical: 20,
+								flexDirection: "row",
+								justifyContent: "center",
+								alignItems: "center",
+							}}
 						>
-							OR
-						</Text>
-						<View style={STYLES.line}></View>
+							<View style={STYLES.line}></View>
+							<Text
+								style={{ marginHorizontal: 5, fontWeight: "bold" }}
+							>
+								OR
+							</Text>
+							<View style={STYLES.line}></View>
+						</View>
+						<View
+							style={{
+								flexDirection: "row",
+								justifyContent: "space-between",
+							}}
+						>
+							<View style={STYLES.btnSecondary}>
+								<TouchableOpacity onPress={() => ""}>
+									<Text
+										style={{ fontWeight: "bold", fontSize: 18 }}
+									>
+										Sign up with
+										<Image
+											style={STYLES.btnImage}
+											source={require("../../assets/facebook.png")}
+										/>
+									</Text>
+								</TouchableOpacity>
+							</View>
+							<View style={{ width: 10 }}></View>
+							<View style={STYLES.btnSecondary}>
+								<TouchableOpacity onPress={() => ""}>
+									<Text
+										style={{ fontWeight: "bold", fontSize: 18 }}
+									>
+										Sign up with
+										<Image
+											style={STYLES.btnImage}
+											source={require("../../assets/google.png")}
+										/>
+									</Text>
+								</TouchableOpacity>
+							</View>
+						</View>
 					</View>
+
 					<View
 						style={{
 							flexDirection: "row",
-							justifyContent: "space-between",
+							alignItems: "flex-end",
+							justifyContent: "center",
+							marginTop: 40,
+							marginBottom: 20,
 						}}
-					>
-						<View style={STYLES.btnSecondary}>
-							<TouchableOpacity onPress={() => ""}>
-								<Text
-									style={{ fontWeight: "bold", fontSize: 18 }}
-								>
-									Sign up with
-									<Image
-										style={STYLES.btnImage}
-										source={require("../../assets/facebook.png")}
-									/>
-								</Text>
-							</TouchableOpacity>
-						</View>
-						<View style={{ width: 10 }}></View>
-						<View style={STYLES.btnSecondary}>
-							<TouchableOpacity onPress={() => ""}>
-								<Text
-									style={{ fontWeight: "bold", fontSize: 18 }}
-								>
-									Sign up with
-									<Image
-										style={STYLES.btnImage}
-										source={require("../../assets/google.png")}
-									/>
-								</Text>
-							</TouchableOpacity>
-						</View>
-					</View>
-				</View>
-
-				<View
-					style={{
-						flexDirection: "row",
-						alignItems: "flex-end",
-						justifyContent: "center",
-						marginTop: 40,
-						marginBottom: 20,
-					}}
-				>
-					<Text
-						style={{
-							color: Colors.grey,
-							fontSize: 18,
-							fontWeight: "bold",
-						}}
-					>
-						Already have an account ?
-					</Text>
-					<TouchableOpacity
-						onPress={() => navigation.navigate("LogIn")}
 					>
 						<Text
 							style={{
-								color: Colors.black,
+								color: Colors.grey,
 								fontSize: 18,
 								fontWeight: "bold",
 							}}
 						>
-							Login
+							Already have an account ?
 						</Text>
-					</TouchableOpacity>
-				</View>
-		<View>
-			<Modal visible={isVisible} >
-				<Text >Enter Your Six digit Code here!</Text>
-				<TextInput placeholder="######" 
-				onChangeText={ text => SetOtp(text) } />
-				<Text >{otpwarning}</Text>
-				<TouchableOpacity onPress={()=>otpSubmit(otp,Setotpwarning)}>
-					<Text style={{ color: Colors.secondary, fontSize: 18,fontWeight: "bold",}}>Submit</Text>
-				</TouchableOpacity>
-			</Modal>
-		</View>
-			</SafeAreaView>
-		</ScrollView>
+						<TouchableOpacity
+							onPress={() => navigation.navigate("LogIn")}
+						>
+							<Text
+								style={{
+									color: Colors.black,
+									fontSize: 18,
+									fontWeight: "bold",
+								}}
+							>
+								Login
+							</Text>
+						</TouchableOpacity>
+					</View>
+					<View>
+						<Modal visible={isVisible} >
+							<Text >Enter Your Six digit Code here!</Text>
+							<TextInput placeholder="######"
+								onChangeText={text => SetOtp(text)} />
+							<Text >{otpwarning}</Text>
+							<TouchableOpacity onPress={() => otpSubmit(otp, Setotpwarning)}>
+								<Text style={{ color: Colors.secondary, fontSize: 18, fontWeight: "bold", }}>Submit</Text>
+							</TouchableOpacity>
+						</Modal>
+					</View>
+				</SafeAreaView>
+			</ScrollView>
 		</>
 	);
 };
@@ -426,26 +426,26 @@ const style = StyleSheet.create({
 });
 
 
-function otpSubmit(otp,setotpwarning){
-	if(isNaN(otp)) {
+function otpSubmit(otp, setotpwarning) {
+	if (isNaN(otp)) {
 		setotpwarning("please enter valid number")
 		return;
 	}
-	AsyncStorage.getItem("signup_token",(err,result)=>{
-		mainBackend.post("/user/confirm/",{token:result,otp:otp})
-		.then(function(response){
-			switch(response.status){
-				case 202:
-					alert("signup success")
-					break;
-				case 410:
-					alert("otp time expired try again")
-				case 400:
-					alert("oops something went wrong!")
-			}
-		})
+	AsyncStorage.getItem("signup_token", (err, result) => {
+		mainBackend.post("/user/confirm/", { token: result, otp: otp })
+			.then(function (response) {
+				switch (response.status) {
+					case 202:
+						alert("signup success")
+						break;
+					case 410:
+						alert("otp time expired try again")
+					case 400:
+						alert("oops something went wrong!")
+				}
+			})
 	});
-	
+
 
 }
 
@@ -543,11 +543,9 @@ function submit(user_Data, log_Setters) {
 		}).then(function (response) {
 			console.log(response.status)
 			if (response.status == 201) {
-				if(AsyncStorage.setItem("signup_token",response.data.signup_token))
-				{log_Setters.SetIsVisible(true);}
+				if (AsyncStorage.setItem("signup_token", response.data.signup_token)) { log_Setters.SetIsVisible(true); }
 			}
-			if (response.status >= 400 && response.status < 500) 
-			{Alert.alert("oops! something went wrong")}
+			if (response.status >= 400 && response.status < 500) { Alert.alert("oops! something went wrong") }
 		})
 	}
 }

24003be8e462a45d7d1c70de06ecb96113693dce
Afzal <sayyed0786afzal@gmail.com>
2021-11-14 02:41:45 +0530

    merge

--- backend/store/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from store.models import Items
-
-
-class ItemsInList(ModelSerializer):
-    class Meta:
-        model = Items
-        fields = ["Name","Price","Display_Image"]
--- backend/userManagement/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from userManagement import models
-
-
-admin.site.register(models.Gender)
-admin.site.register(models.UnVerifiedUser)
-admin.site.register(models.Users)
